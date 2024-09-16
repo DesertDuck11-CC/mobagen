5d259bd4c283519f92baf28c6dc0757de388188f
DesertDuck11-CC <143830623+DesertDuck11-CC@users.noreply.github.com>
2024-09-15 22:58:43 -0400

    Add files via upload

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -1,7 +1,72 @@
-//
-// Created by atolstenko on 2/9/2023.
-//
-
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+
+void HexagonGameOfLife::Step(World& world) 
+{
+  for (size_t i = 0; i < world.SideSize(); i++) {
+    for (size_t j = 0; j < world.SideSize(); j++) {
+      Point2D point(i, j);
+
+      int neighbours = CountNeighbors(world, point);
+
+      if (world.Get(point) && neighbours < 2) {
+        world.SetNext(point, false);  // Underpopulation
+      } else if (world.Get(point) && (neighbours == 2 || neighbours == 3)) {
+        world.SetNext(point, true);  // Survives
+      } else if (world.Get(point) && neighbours > 3) {
+        world.SetNext(point, false);  // Overpopulation
+      } else if (!world.Get(point) && neighbours == 3) {
+        world.SetNext(point, true);  // Reproduction
+      }
+    }
+  }
+}
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) 
+{
+  int sum = 0;
+ 
+  //Even
+  if (point.y % 2 == 0) 
+  {
+    if (world.Get(point + point.RIGHT)) {
+      sum++;
+    }
+    if (world.Get(point + point.UP)) {
+      sum++;
+    }
+    if (world.Get(point + point.UP + point.LEFT)) {
+      sum++;
+    }
+    if (world.Get(point + point.LEFT)) {
+      sum++;
+    }
+    if (world.Get(point + point.DOWN + point.LEFT)) {
+      sum++;
+    }
+    if (world.Get(point + point.DOWN)) {
+      sum++;
+    }
+  }
+  else  //Odd
+  {
+    if (world.Get(point + point.RIGHT)) {
+      sum++;
+    }
+    if (world.Get(point + point.UP + point.RIGHT)) {
+      sum++;
+    }
+    if (world.Get(point + point.UP)) {
+      sum++;
+    }
+    if (world.Get(point + point.LEFT)) {
+      sum++;
+    }
+    if (world.Get(point + point.DOWN)) {
+      sum++;
+    }
+    if (world.Get(point + point.DOWN + point.RIGHT)) {
+      sum++;
+    }
+  }
+
+  return sum; 
+}
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -2,10 +2,67 @@
 
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-  // todo: implement
+  for (size_t i = 0; i < world.SideSize(); i++) 
+  {
+    for (size_t j = 0; j < world.SideSize(); j++) 
+    {
+      Point2D point(i, j);
+
+      int neighbours = CountNeighbors(world, point);
+
+      if (world.Get(point) && neighbours < 2)
+      {
+        world.SetNext(point, false); //Underpopulation
+      }
+      else if (world.Get(point) && (neighbours == 2 || neighbours == 3))
+      {
+        world.SetNext(point, true); //Survives
+      }
+      else if (world.Get(point) && neighbours > 3) {
+        world.SetNext(point, false); //Overpopulation
+      }
+      else if (!world.Get(point) && neighbours == 3) {
+        world.SetNext(point, true); //Reproduction
+      }
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  
-  return 0;
+  int sum = 0;
+
+  //Check Cardinals
+  if (world.Get(point + point.UP))
+  {
+    sum++;
+  }
+  if (world.Get(point + point.DOWN))
+  {
+    sum++;
+  }
+  if (world.Get(point + point.LEFT))
+  {
+    sum++;
+  }
+  if (world.Get(point + point.RIGHT)) 
+  {
+    sum++;
+  }
+
+  //Check Diagonals
+  if (world.Get(point + point.UP + point.LEFT))
+  {
+      sum++;
+  }
+  if (world.Get(point + point.UP + point.RIGHT)) {
+      sum++;
+  }
+  if (world.Get(point + point.DOWN + point.LEFT)) {
+      sum++;
+  }
+  if (world.Get(point + point.DOWN + point.RIGHT)) {
+      sum++;
+  }
+
+  return sum;
 }
