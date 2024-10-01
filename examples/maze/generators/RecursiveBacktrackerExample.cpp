fa1cf0b0c807e9afeed8840c3c39e7e51d83611f
DesertDuck11 <benjamin.trudell@mymail.champlain.edu>
2024-10-01 12:50:15 -0400

    done

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,16 +4,15 @@
 #include <climits>
 #include <vector>
 #include <stack>
-bool RecursiveBacktrackerExample::Step(World* w) {
-    
-    stack.push_back(Point2D(0, 0));
-    
+bool RecursiveBacktrackerExample::Step(World* w) {    
     
     if (!stack.empty()) 
     {
-        Point2D current = stack.front();
+        Point2D current = stack.back();
 
         visited[current.x][current.y] = true;
+
+        w->SetNodeColor(current, Color::Red);
 
         std::vector<Point2D> neighbours = getVisitables(w, current);
         if (neighbours.size() > 0)
@@ -25,16 +24,48 @@ bool RecursiveBacktrackerExample::Step(World* w) {
             }
             else
             {
-                target = randomStartPoint(w);
+                Random rand;
+                target = neighbours[rand.Range(0, neighbours.size() - 1)];
             }
+
+            if (target == stack.back().Right())
+            {
+                w->SetEast(stack.back(), false);
+                w->SetWest(stack.back().Right(), false);
+            }
+            else if (target == stack.back().Up()) 
+            {
+                w->SetNorth(stack.back(), false);
+                w->SetSouth(stack.back().Up(), false);
+            }
+            else if (target == stack.back().Down()) 
+            {
+                w->SetSouth(stack.back(), false);
+                w->SetNorth(stack.back().Down(), false);
+            } 
+            else if (target == stack.back().Left()) 
+            {
+                w->SetWest(stack.back(), false);
+                w->SetEast(stack.back().Left(), false);
+            }
+
+            stack.push_back(target);
+
+            return true;
+        }
+        else
+        {
+            w->SetNodeColor(stack.back(), Color::Black);
+            stack.pop_back();
+
+            return true;
         }
 
     } 
     else 
     {
-        stack.pop_back();
+        stack.push_back(randomStartPoint(w));
     }
-
 
     return false;
 }
@@ -64,6 +95,42 @@ Point2D RecursiveBacktrackerExample::randomStartPoint(World* world) {
 std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const Point2D& p) {
     auto sideOver2 = w->GetSize() / 2;
     std::vector<Point2D> visitables;
+
+    //Up
+    if ((p.Up().x >= -sideOver2) && (p.Up().x <= sideOver2) && (p.Up().y <= sideOver2) && (p.Up().y >= -sideOver2))
+    {
+        if (!visited[p.Up().x][p.Up().y]) 
+        {
+            visitables.push_back(p.Up());
+        }
+    }
+
+    //Right
+    if ((p.Right().x >= -sideOver2) && (p.Right().x <= sideOver2) && (p.Right().y <= sideOver2) && (p.Right().y >= -sideOver2)) 
+    {
+        if (!visited[p.Right().x][p.Right().y]) 
+        {
+            visitables.push_back(p.Right());
+        }
+    }
+
+    //Down
+    if ((p.Down().x >= -sideOver2) && (p.Down().x <= sideOver2) && (p.Down().y <= sideOver2) && (p.Down().y >= -sideOver2)) 
+    {
+        if (!visited[p.Down().x][p.Down().y]) 
+        {
+            visitables.push_back(p.Down());
+        }
+    }
+
+    //Left
+    if ((p.Left().x >= -sideOver2) && (p.Left().x <= sideOver2) && (p.Left().y <= sideOver2) && (p.Left().y >= -sideOver2)) 
+    {
+        if (!visited[p.Left().x][p.Left().y]) 
+        {
+            visitables.push_back(p.Left());
+        }
+    }
 
     return visitables;
 }
